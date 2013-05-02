8f1f8282738a32f6b2131c7311ad04f00b3ae755
krolique <nick@digitalscripter.com>
2013-05-01 23:03:27 -0400

    Add problem #4 solution
    Added solution to problem #4, which is to find the highest palindrome by multiplying 3-digit numbers together. I am NOT happy with the run time of this solution.... O(n^2)......

---
+++ problems/4_largest_palindrone_product.py
@@ -0,0 +1,32 @@
+"""
+
+    Problem 4
+    =========
+
+    Largest palindrome product
+
+    A palindromic number reads the same both ways. The largest palindrome
+    made from the product of two 2-digit numbers is 9009 = 91 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+
+"""
+#: palindrome digit size
+n = 3
+
+
+def problem(n=n):
+    """ Attempt to solve the problem... """
+
+    print 'problem #4'
+    biggest = 0
+    for i in xrange(10**n):
+        for j in xrange(10**n):
+            product = i*j
+            if str(product) == str(product)[::-1] and product > biggest:
+                biggest = product
+
+    print 'the biggest palindrome of %d-digit product is %d' % (n, biggest)
+
+if __name__ == "__main__":
+    problem()
