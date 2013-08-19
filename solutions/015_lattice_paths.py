9869cc93a1f873b3c07ad82884554d09ec0e24fc
krolique <nick@digitalscripter.com>
2013-08-19 01:18:46 -0400

    Add solutions to problem #15 and #16
    - Added solution to problem #15
    
    - Added solution to problem #16

--- solutions/015_lattice_paths.py
+++ solutions/015_lattice_paths.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 
-    Problem #15
-    -----------
+    Problem #15 - Lattice paths
+    ---------------------------
 
     Starting in the top left corner of a 2×2 grid, and only being able to
     move to the right and down, there are exactly 6 routes to the bottom
@@ -15,12 +15,20 @@
 
 """
 
+from math import factorial
+
+n = 20
+
 
 def problem():
     """ Attempt to solve the problem... """
 
     print 'problem #15'
 
+    # because we are restricted to left and down moves the problems comes down
+    # to counting 2n (left and right) path choose n
+    number_of_paths = factorial(2*n)/(factorial(n)**2)
+    print 'the number of paths to the lattice is %s' % number_of_paths
 
 if __name__ == "__main__":
 
