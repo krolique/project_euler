90b6c7088115e81c931897ae8fa3759fbcb40001
krolique <nick@digitalscripter.com>
2013-08-31 18:20:40 -0400

    Add common module
    Added permutation function for lists

---
+++ common.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+	Common
+	------
+
+	Contains shared code 
+
+"""
+
+def permutations(seq, permutation=[]):
+    """Returns a permutations generator
+
+    Each `__next__` call will return the next permutation of a given sequence
+
+    :param seq: sequence of characters to be permuted
+    :type seq: list
+    :param permutation: used to store carried over sequence characters during
+    					recursion
+    :type permutation: str
+    :returns: generator
+    """
+    if not seq:
+        yield permutation
+    for i, v in enumerate(seq):
+        for p in permutations(seq[0:i] + seq[i+1:], permutation + [seq[i]]):
+            yield p
